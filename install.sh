202da05b0d8614f84a09aa90cd0abfcad5b049e8
Yuval <smp.yuval@gmail.com>
2018-09-17 17:59:40 +0300

    introduced change_all_bits()

--- install.sh
+++ install.sh
@@ -17,6 +17,8 @@ source ../functions
 
 appname=examples_TBD
 
+
+
 #DV=2.0-090212
 #appname=examples
 #RPM_LIST="$appname-$DV.x86_64.rpm"
@@ -43,7 +45,7 @@ appname=examples_TBD
 #################################################################
 pre() {
 
-    if [ "$1" -eq "1" ]; then
+    if [ "$1" = "1" ]; then
             msg="$(
                     echo "this is an update"
                     echo "uninstalling previous installation"
@@ -70,7 +72,7 @@ pre() {
 #		return $RET_INST_INST
 #	fi
 
-    # TODO: Need to add a test we are on RHEL7
+    # TODO: Shai commented that need test we are on RHEL7. Actually not clear why.
 
     if [ $(cat /proc/sys/kernel/sched_domain/cpu0/domain*/name | egrep -c -e "NUMA|ALL") -lt 2 ]; then
         local msg="Scheduler Domain tree does not require changes."
@@ -108,26 +110,35 @@ calc_new_flags_value() {
 
 }
 
-install() {
-    echo "Installing $appname:"
+change_all_bits() {
+    # change_all_bits manipulates flag's bits.
+    # Input:
+    #   bits_action: as required by calc_new_flags_value()
 
-    num_cpus=$(grep -c processor /proc/cpuinfo)
+    bits_action=$1
+
+    NUM_CPUS=$(grep -c processor /proc/cpuinfo)
+    # TODO: SD_LOAD_BALANCE not used - ask tal.
     SD_LOAD_BALANCE=$((0x0001))
     SD_BALANCE_NEWIDLE=$((0x0002))
     SD_SERIALIZE=$((0x0400))
 
-    # TODO: The following is never used
+
+    # TODO: 'domains' is never used - confirm again
     domains=$(ls /proc/sys/kernel/sched_domain/cpu0 | wc -l)
 
     system_domain=$(ls /proc/sys/kernel/sched_domain/cpu0 | tail -n 1)
     board_domain=$(ls /proc/sys/kernel/sched_domain/cpu0 | tail -n 2 | head -n 1)
 
-    for cpu in $(seq 0 $((num_cpus-1))) ; do
+    for cpu in $(seq 0 $((NUM_CPUS-1))) ; do
         cur_flags=$(cat /proc/sys/kernel/sched_domain/cpu$cpu/$system_domain/flags)
+        # TODO - tal touched the same domain in two step. Verify that he actually intended the same domain and not a typo.
         sys_dom_flags=$((SD_BALANCE_NEWIDLE | SD_SERIALIZE))
-        upd_flags=$(calc_new_flags_value clear ${cur_flags} ${sys_dom_flags})
+        upd_flags=$(calc_new_flags_value ${bits_action} ${cur_flags} ${sys_dom_flags})
 
-        # TODO: Need to add debug printings for the tow methods. Confirm it match and reversible.
+        #TODO: DEBUG: only
+        cur_flags0=$cur_flags
+        upd_flags0=$upd_flags
 
 #        echo "Changing system domain flags on CPU $cpu from $FLAGS to $((FLAGS & ~SD_BALANCE_NEWIDLE))"
 #        sudo bash -c "echo $((FLAGS & ~SD_BALANCE_NEWIDLE)) >/proc/sys/kernel/sched_domain/cpu$cpu/$system_domain/flags"
@@ -137,13 +148,24 @@ install() {
 #        sudo bash -c "echo $((FLAGS & ~SD_SERIALIZE)) >/proc/sys/kernel/sched_domain/cpu$cpu/$system_domain/flags"
 
         cur_flags=$(cat /proc/sys/kernel/sched_domain/cpu$cpu/$board_domain/flags)
-        upd_flags=$(calc_new_flags_value clear ${cur_flags} ${SD_SERIALIZE})
+        upd_flags=$(calc_new_flags_value ${bits_action} ${cur_flags} ${SD_SERIALIZE})
 
+        printf "0x%x " $SD_SERIALIZE
+
+        printf "DEBUG: %03d  0x%x -> 0x%x ; 0x%x -> 0x%x  \n" $cpu $cur_flags0 $upd_flags0 $cur_flags $upd_flags
+
+        exit
 
 #        FLAGS=$(cat /proc/sys/kernel/sched_domain/cpu$cpu/$board_domain/flags)
 #        echo "Changing board  domain flags on CPU $cpu from $FLAGS to $((FLAGS & ~SD_SERIALIZE))"
 #        sudo bash -c "echo $((FLAGS & ~SD_SERIALIZE)) >/proc/sys/kernel/sched_domain/cpu$cpu/$board_domain/flags"
     done
+}
+
+install() {
+    echo "Installing $appname:"
+
+    change_all_bits clear
 
 	return $RET_INST_SUCCESS
 
@@ -160,7 +182,7 @@ post() {
 
 uninstall() {
 
-	UNINSTALL_LIST="$RPM_LIST"
+#	UNINSTALL_LIST="$RPM_LIST"
 
 	echo "Uninstalling $appname:"
 
